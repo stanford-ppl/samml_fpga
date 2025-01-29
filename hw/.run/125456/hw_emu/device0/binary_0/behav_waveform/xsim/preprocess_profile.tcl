25cb981defe98f938808cf5f155b3581db888fc4
Ubuntu <ubuntu@ip-172-31-45-23.ec2.internal>
2025-01-29 23:12:16 +0000

    Working example code

---
+++ hw/.run/125456/hw_emu/device0/binary_0/behav_waveform/xsim/preprocess_profile.tcl
@@ -0,0 +1,113 @@
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/entry_proc_U0/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_U0/grp_read_input_Pipeline_mem_rd_fu_82/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/read_input_1_U0/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/compute_add_U0/grp_compute_add_Pipeline_execute_fu_52/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_idle
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_idle -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_int_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_int_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_ext_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_ext_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_str_blocking_n
+get_value_database /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst/write_result_U0/grp_write_result_Pipeline_mem_wr_fu_73/ap_str_blocking_n -time 0 -quiet 
+
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst//size_c1_U/mOutPtr
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst//inStream1_U/mOutPtr
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst//inStream2_U/mOutPtr
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst//size_c_U/mOutPtr
+log_wave -quiet /pfm_top_wrapper/pfm_top_i/pfm_dynamic_inst/vadd_1/inst//outStream_U/mOutPtr
---
+++ hw/.run/125456/hw_emu/device0/binary_0/behav_waveform/xsim/xsim.dir/pfm_top_wrapper_behav/obj/sysc_interface.h
@@ -0,0 +1,195 @@
+#include "pfm_dynamic_auto_cc_1.h"
+
+pfm_dynamic_auto_cc_1 *sc_inst_1;
+#include "pfm_dynamic_m00_regslice_0.h"
+
+pfm_dynamic_m00_regslice_0 *sc_inst_2;
+#include "pfm_dynamic_auto_cc_2.h"
+
+pfm_dynamic_auto_cc_2 *sc_inst_3;
+#include "pfm_dynamic_m01_regslice_7.h"
+
+pfm_dynamic_m01_regslice_7 *sc_inst_4;
+#include "pfm_dynamic_auto_cc_3.h"
+
+pfm_dynamic_auto_cc_3 *sc_inst_5;
+#include "pfm_dynamic_m02_regslice_7.h"
+
+pfm_dynamic_m02_regslice_7 *sc_inst_6;
+#include "pfm_dynamic_auto_cc_4.h"
+
+pfm_dynamic_auto_cc_4 *sc_inst_7;
+#include "pfm_dynamic_m03_regslice_0.h"
+
+pfm_dynamic_m03_regslice_0 *sc_inst_8;
+#include "pfm_dynamic_s00_regslice_0.h"
+
+pfm_dynamic_s00_regslice_0 *sc_inst_9;
+#include "pfm_dynamic_xbar_6.h"
+
+pfm_dynamic_xbar_6 *sc_inst_10;
+#include "pfm_dynamic_dpa_hub_0.h"
+
+pfm_dynamic_dpa_hub_0 *sc_inst_11;
+#include "pfm_dynamic_dpa_mon0_0.h"
+
+pfm_dynamic_dpa_mon0_0 *sc_inst_12;
+#include "pfm_dynamic_dpa_mon1_0.h"
+
+pfm_dynamic_dpa_mon1_0 *sc_inst_13;
+#include "bd_5dca_sci.h"
+
+bd_5dca_sci *sc_inst_14;
+#include "bd_5dca_hbm_inst_0.h"
+
+bd_5dca_hbm_inst_0 *sc_inst_15;
+#include "bd_5dca_init_concat_0.h"
+
+bd_5dca_init_concat_0 *sc_inst_16;
+#include "bd_5dca_interconnect1_0_0.h"
+
+bd_5dca_interconnect1_0_0 *sc_inst_17;
+#include "bd_5dca_slice1_0_0.h"
+
+bd_5dca_slice1_0_0 *sc_inst_18;
+#include "bd_5dca_interconnect2_1_0.h"
+
+bd_5dca_interconnect2_1_0 *sc_inst_19;
+#include "bd_5dca_slice2_1_0.h"
+
+bd_5dca_slice2_1_0 *sc_inst_20;
+#include "bd_5dca_interconnect0_2_0.h"
+
+bd_5dca_interconnect0_2_0 *sc_inst_21;
+#include "bd_5dca_slice0_2_0.h"
+
+bd_5dca_slice0_2_0 *sc_inst_22;
+#include "bd_5dca_vip_S00_0.h"
+
+bd_5dca_vip_S00_0 *sc_inst_23;
+#include "bd_5dca_vip_S01_0.h"
+
+bd_5dca_vip_S01_0 *sc_inst_24;
+#include "bd_5dca_vip_S02_0.h"
+
+bd_5dca_vip_S02_0 *sc_inst_25;
+#include "pfm_dynamic_xlconcat_interrupt_0.h"
+
+pfm_dynamic_xlconcat_interrupt_0 *sc_inst_26;
+#include "pfm_dynamic_xlconcat_interrupt_0_0.h"
+
+pfm_dynamic_xlconcat_interrupt_0_0 *sc_inst_27;
+#include "pfm_dynamic_xlconcat_interrupt_1_0.h"
+
+pfm_dynamic_xlconcat_interrupt_1_0 *sc_inst_28;
+#include "pfm_dynamic_xlconcat_interrupt_2_0.h"
+
+pfm_dynamic_xlconcat_interrupt_2_0 *sc_inst_29;
+#include "pfm_dynamic_xlconcat_interrupt_3_0.h"
+
+pfm_dynamic_xlconcat_interrupt_3_0 *sc_inst_30;
+#include "pfm_dynamic_irq_const_tieoff_0.h"
+
+pfm_dynamic_irq_const_tieoff_0 *sc_inst_31;
+#include "bd_d216_sci.h"
+
+bd_d216_sci *sc_inst_32;
+#include "bd_d216_interconnect_M00_AXI_MEM00_0.h"
+
+bd_d216_interconnect_M00_AXI_MEM00_0 *sc_inst_33;
+#include "bd_d216_interconnect_M01_AXI_MEM00_0.h"
+
+bd_d216_interconnect_M01_AXI_MEM00_0 *sc_inst_34;
+#include "bd_d216_interconnect_PLRAM_MEM00_0.h"
+
+bd_d216_interconnect_PLRAM_MEM00_0 *sc_inst_35;
+#include "bd_d216_interconnect_PLRAM_MEM01_0.h"
+
+bd_d216_interconnect_PLRAM_MEM01_0 *sc_inst_36;
+#include "bd_d216_interconnect_PLRAM_MEM02_0.h"
+
+bd_d216_interconnect_PLRAM_MEM02_0 *sc_inst_37;
+#include "bd_d216_interconnect_PLRAM_MEM03_0.h"
+
+bd_d216_interconnect_PLRAM_MEM03_0 *sc_inst_38;
+#include "bd_d216_interconnect_PLRAM_MEM04_0.h"
+
+bd_d216_interconnect_PLRAM_MEM04_0 *sc_inst_39;
+#include "bd_d216_interconnect_PLRAM_MEM05_0.h"
+
+bd_d216_interconnect_PLRAM_MEM05_0 *sc_inst_40;
+#include "bd_d216_interconnect_S00_AXI_0.h"
+
+bd_d216_interconnect_S00_AXI_0 *sc_inst_41;
+#include "bd_d216_rs_M00_AXI_0.h"
+
+bd_d216_rs_M00_AXI_0 *sc_inst_42;
+#include "bd_d216_rs_M01_AXI_0.h"
+
+bd_d216_rs_M01_AXI_0 *sc_inst_43;
+#include "bd_d216_plram_mem00_0.h"
+
+bd_d216_plram_mem00_0 *sc_inst_44;
+#include "bd_d216_plram_mem01_0.h"
+
+bd_d216_plram_mem01_0 *sc_inst_45;
+#include "bd_d216_plram_mem02_0.h"
+
+bd_d216_plram_mem02_0 *sc_inst_46;
+#include "bd_d216_plram_mem03_0.h"
+
+bd_d216_plram_mem03_0 *sc_inst_47;
+#include "bd_d216_plram_mem04_0.h"
+
+bd_d216_plram_mem04_0 *sc_inst_48;
+#include "bd_d216_plram_mem05_0.h"
+
+bd_d216_plram_mem05_0 *sc_inst_49;
+#include "pfm_dynamic_sci.h"
+
+pfm_dynamic_sci *sc_inst_50;
+#include "pfm_dynamic_profile_vip_0_0.h"
+
+pfm_dynamic_profile_vip_0_0 *sc_inst_51;
+#include "pfm_dynamic_m01_regslice_6.h"
+
+pfm_dynamic_m01_regslice_6 *sc_inst_52;
+#include "pfm_dynamic_auto_cc_0.h"
+
+pfm_dynamic_auto_cc_0 *sc_inst_53;
+#include "pfm_dynamic_m02_regslice_6.h"
+
+pfm_dynamic_m02_regslice_6 *sc_inst_54;
+#include "pfm_dynamic_xbar_5.h"
+
+pfm_dynamic_xbar_5 *sc_inst_55;
+#include "pfm_dynamic_smartconn_data_0_0.h"
+
+pfm_dynamic_smartconn_data_0_0 *sc_inst_56;
+#include "pfm_top_sci.h"
+
+pfm_top_sci *sc_inst_57;
+#include "pfm_top_axi_clock_converter_0_0.h"
+
+pfm_top_axi_clock_converter_0_0 *sc_inst_58;
+#include "pfm_top_axi_vip_0_0.h"
+
+pfm_top_axi_vip_0_0 *sc_inst_59;
+#include "pfm_top_xbar_0.h"
+
+pfm_top_xbar_0 *sc_inst_60;
+#include "pfm_top_embedded_schedular_0.h"
+
+pfm_top_embedded_schedular_0 *sc_inst_61;
+#include "pfm_top_sim_address_0_0.h"
+
+pfm_top_sim_address_0_0 *sc_inst_62;
+#include "pfm_top_sim_ddr_0_0.h"
+
+pfm_top_sim_ddr_0_0 *sc_inst_63;
+#include "pfm_top_sim_qdma_0_0.h"
+
+pfm_top_sim_qdma_0_0 *sc_inst_64;
+#include "pfm_top_smartconnect_0_0.h"
+
+pfm_top_smartconnect_0_0 *sc_inst_65;
